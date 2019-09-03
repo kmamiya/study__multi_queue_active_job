c87851dd2ea63bdd59b617c9bef33f9d59855bd4
Kentarou Mamiya <kmamiya@logicalrabbit.jp>
2019-09-03 15:29:09 +0900

    実行開始前に前回残ったpidファイルを削除する

--- docker/rails/cmd.sh
+++ docker/rails/cmd.sh
@@ -1,8 +1,13 @@
 #bin/env bash
 
+# docker-composeを止めた時 tmp/pids 内がうまく消えない(各プロセス終了前にcontainerが止まっている？)ので強制的に削除しておく
+rm -f tmp/pids/*.pid tmp/pids/worker1/*.pid tmp/pids/worker2/*.pid
 
 # Delayed job
 # こちらはバックグラウンド起動する
+# worker_name はループ処理などにしたいが一先ず毎回変数をセットする形式
+# worker1は長時間重プロセス想定、同時実行数を減らしてかつ優先度低
+# worker2は短時間軽プロセス想定、同寺実行数を増やして優先度も高め
 worker_name='worker1'
 bin/delayed_job --queue=queue1 --prefix ${worker_name} --pid-dir=tmp/pids/${worker_name} -n 4 --max-priority 10 start
 worker_name='worker2'
