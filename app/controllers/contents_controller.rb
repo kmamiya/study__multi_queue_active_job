5acea282fa797bc092bdb03d138e11b7160ad98f
Kentarou Mamiya <kmamiya@logicalrabbit.jp>
2019-09-03 14:23:37 +0900

    キューを指定して10分待機するだけのジョブを投げる

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,3 +1,8 @@
 class ContentsController < ApplicationController
   def index ; end
+
+  def create
+    TenMinutesWaitJob.set(queue: params[:queue]).perform_later
+    redirect_to contents_path
+  end
 end
